79bad0fcc09a2b64a46057ddba958f2ab83ae72d
petidesoftwares <petideenterprise@gmail.com>
2021-08-06 09:33:36 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route} from "react-router-dom";
 import Register from "./register";
+import axios from "axios";
+
+axios.defaults.baseURL="http://127.0.0.1:8000";
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
+// axios.defaults.headers.post['Accept'] = 'application/json';
+
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
--- src/register.js
+++ src/register.js
@@ -1,26 +1,45 @@
 import ReactDOM from 'react-dom';
 import './App.css';
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
-class Register extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
+function Register (){
+        const [inputStatus,setInput] = useState({
             name: "",
             email: "",
             password: ""
-        }
+        })
+    const handleInput=(e)=>{
+        e.persist();
+        setInput({...inputStatus,[e.target.name]:[e.target.value]})
     }
-render() {
+    const registerUser=(e)=>{
+            e.preventDefault();
+            const regData = {
+                name:inputStatus.name[0],
+                email:inputStatus.email[0],
+                password:inputStatus.password[0]
+            }
+        fetch('http://127.0.0.1:8000/sanctum/csrf-cookie').then(response=>response.json()).then((resp)=>{
+            console.log(resp);
+        }, (error) =>{
+            console.log(error);
+        })
+        // then(response => {
+        // //     axios.post('api/register',regData).then(response=>{
+        // //         console.log(response);
+        //     })
+        // })
+    }
     return(
             <div>
-                <form>
-                    <input type="text" name="name"/>
-                    <input type="text" name="email"/>
-                    <input type="password" name="password"/>
+                <form onSubmit={registerUser}>
+                    <input type="text" name="name" placeholder="Enter Name" onChange={handleInput} value={inputStatus.name}/><br/>
+                    <input type="text" name="email" placeholder="Enter Email" onChange={handleInput} value={inputStatus.email}/><br/>
+                    <input type="password" name="password" placeholder={"Enter Password"} onChange={handleInput} value={inputStatus.password}/><br/>
+                    <input type="submit" name="submit" value="Register"/>
                 </form>
             </div>
         )
-    }
 }
 export default Register;
